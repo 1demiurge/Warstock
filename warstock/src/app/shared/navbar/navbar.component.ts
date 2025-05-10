0b9ca88a8cd7f16146bd4f1b83d1549543a6733d
Rajarshi Agarwal <72816727+King-Agashi@users.noreply.github.com>
2025-05-10 13:25:38 +0000

    modifications to navbar

--- warstock/src/app/shared/navbar/navbar.component.ts
+++ warstock/src/app/shared/navbar/navbar.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
 export class NavbarComponent {
-  menuOpen: boolean = false;
+  menuOpen = false;
 
   toggleMenu() {
     this.menuOpen = !this.menuOpen;
@@ -15,5 +14,13 @@ export class NavbarComponent {
 
   closeMenu() {
     this.menuOpen = false;
+  }
+
+  scrollToSection(sectionId: string) {
+    const section = document.getElementById(sectionId);
+    if (section) {
+      section.scrollIntoView({ behavior: 'smooth', block: 'start' });
+      this.closeMenu(); // Close the menu if it's open
+    }
   }
 }
